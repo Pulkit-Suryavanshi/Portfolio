7bbf3149eb3f45cee9cf75f9649cb7ace12c9e68
Pulkit-Suryavanshi <47458898+Pulkit-Suryavanshi@users.noreply.github.com>
2020-07-23 01:33:22 +0530

    Add files via upload

---
+++ Pulkit Web devCAPSTONE/README.md
@@ -0,0 +1,23 @@
+## Capstone Project
+
+Coursera UMICH Web Design for Everybody Capstone Final Project: Create a Personal Portfolio
+
+## Requirements
+
+* Site Validates
+* Site is responsive
+* Site is well-styled
+* Images are incorporated effectively
+* Successful demonstration of ability to enhance site
+
+## Technologies Used
+* HTML5
+* CSS3
+* JavaScript
+* jQuery
+
+## Deployment
+Open index.html
+
+## Launch
+[Portfolio](https://ziggysauce.github.io/WDE-capstone/index.html)
---
+++ Pulkit Web devCAPSTONE/js/main.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+  navToggle();
+  smoothScroll();
+  if ($(window).width() >= 700) {
+    $(document).on("scroll", highlightNav);
+  }
+});
+
+// Create separate vertical navigation tab for smaller screen view
+function navToggle() {
+  $('.navicon').click(function() {
+    $('.navbar').addClass(' mobile');
+    $('.navbuttons').addClass(' response');
+    $('.navbuttons li').addClass(' showme');
+    $('.navbar p').hide();
+    $('.navicon').addClass("active");
+    $('a').click(function() {
+      $('.navbar').removeClass(' mobile');
+      $('.navbuttons').removeClass(' response');
+      $('.navbuttons li').removeClass(' showme');
+      $('.navbar p').show();
+      $('.navicon').removeClass("active");
+      navToggle();
+    });
+  });
+};
+
+// Smooth scoll transition when any tab is clicked
+function smoothScroll() {
+  $('.scroll').click(function(dest){
+    dest.preventDefault();
+    $('html, body').animate({scrollTop:$(this.hash).offset().top}, 500)
+  });
+};
+
+// Show current active tab for user
+function highlightNav() {
+  var scrollPosition = $(document).scrollTop();
+  $('.scroll').each(function() {
+    var currentId = $(this);
+    var ref = $(currentId.attr('href'));
+    if (ref.offset().top <= scrollPosition) {
+      $('.navbuttons a').removeClass('activenav');
+      currentId.addClass(' activenav');
+    }
+    else {
+      currentId.removeClass('activenav');
+    }
+  });
+};
